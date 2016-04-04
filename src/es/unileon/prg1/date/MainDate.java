b677b6d713e7c3bdfd812b4e16fde8e2460796af
e5mbargl00 <e5mbargl00@pc32.(none)>
2016-04-04 14:27:14 +0200

    Date.java

---
+++ src/es/unileon/prg1/date/MainDate.java
@@ -0,0 +1,11 @@
+/*Path*/
+package es.unileon.prg1.date;
+
+class Main Date{
+	public static void main(String args[]){
+		Date today tomorrow;
+		today = new.Date(21,3,2016);
+		tomorrow = new.Date(22, 3, 2016);
+		today.isSameYear(tomorrow);
+		}
+} 
